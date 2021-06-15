3b4f26344544780ed47a1bfa7b319ea7f3cfd310
potetakoyaki <potetakoyaki@gmail.com>
2021-06-16 00:56:05 +0900

    java学習

---
+++ 配列.java
@@ -0,0 +1,24 @@
+class Arr1{
+  public static void main(String[] args) {
+    String[] arr;
+    arr = new String[3];
+
+    arr[0]="sato";
+    arr[1]="suzuki";
+    arr[2]="takahashi";
+
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[2]);
+  }
+}
+
+class Arr2{
+  public static void main(String[]args) {
+    String[] arr = {"sato","suzuki","takahashi"};
+    
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[2]);
+  }
+}
