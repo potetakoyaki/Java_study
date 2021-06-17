02dd7b077d1c4d769096963b18b80e0f88c16998
potetakoyaki <potetakoyaki@gmail.com>
2021-06-18 08:44:58 +0900

    java学習

---
+++ 課題.java
@@ -0,0 +1,27 @@
+//課題1-1
+class  Main1{
+  public static void main(String[]args){
+    int sum = 0;
+    
+    for(int i = 1; i <= 100; i ++){
+			sum += i;
+		}
+		
+		System.out.println(sum);
+  }
+}
+
+//課題２
+class  Main2{
+  public static void main(String[] args) {
+    int sum = 0;
+    
+    for(int i = 1; i <= 100; i ++){
+      if(i%2==0){
+      sum += i;
+		  }
+    }
+
+		System.out.println(sum);
+  }
+}
