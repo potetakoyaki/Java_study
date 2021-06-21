518a04c055a422cd8f194e948e7ef67c36e6cc4e
potetakoyaki <potetakoyaki@gmail.com>
2021-06-21 20:07:35 +0900

    java学習

---
+++ 学習.java
@@ -0,0 +1,16 @@
+class Main {
+  //比較演算
+  public static void main(String[] args) {
+    System.out.println(true);
+
+    System.out.println(false);
+
+    System.out.println(12 / 4 == 3);
+
+    System.out.println(12 / 4 != 3);
+   
+    boolean bool =  (3*9 ==  27);
+
+    System.out.println(bool);
+  }
+}
