c7c01f420197183daaa23a74ac95d9923e68091c
potetakoyaki <potetakoyaki@gmail.com>
2021-06-27 01:15:00 +0900

    java学習

--- study.java
+++ study.java
@@ -88,3 +88,15 @@ class Study7 {
     System.out.println("私の名前は" + name + "です");
   }
 }
+
+// 複数の引数
+class Study8 {
+  public static void main(String[] args) {
+    printData("Kate Jones",27);
+    printData("John Christopher Smith",65);
+  }
+  public static void printData(String name, int age){
+    System.out.println("私の名前は"+ name + "です");
+    System.out.println("年齢は" + age + "です");
+  }
+}
